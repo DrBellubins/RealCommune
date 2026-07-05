61d1d2f2901478ceb88d3bbe0434494a4bd43802
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-07-04 20:12:44 -0400

    Add maplibre

---
+++ Commune/src/lib/components/MapView.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import maplibregl from 'maplibre-gl';
+  import 'maplibre-gl/dist/maplibre-gl.css';
+
+  let container: HTMLDivElement;
+  let map: maplibregl.Map | undefined;
+
+  onMount(() => {
+    map = new maplibregl.Map({
+      container,
+      style: 'https://demotiles.maplibre.org/style.json',
+      center: [12.5, 41.9],
+      zoom: 13
+    });
+
+    return () => {
+      map?.remove();
+    };
+  });
+</script>
+
+<div bind:this={container} class="map"></div>
+
+<style>
+  .map {
+    width: 100%;
+    height: 70vh;
+    border-radius: 12px;
+  }
+</style>
--- Commune/src/routes/demo/+page.svelte
+++ Commune/src/routes/demo/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+    import MapView from '$lib/components/MapView.svelte';
 </script>
 
-<a href={resolve('/demo/better-auth')}>better-auth</a>
+<MapView />
