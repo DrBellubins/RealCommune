416844ebcb7c476fb18489e720e0c24f8335412f
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-07-14 17:08:16 -0400

    Rename and fix server

--- Commune/src/routes/api/chat/+server.ts
+++ Commune/src/routes/api/chat/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
+import type { RequestHandler } from '../$types';
 import { loadConversation, saveMessage } from '$lib/server/db/ChatDB';
 
 export const GET: RequestHandler = async ({ url }) => {
